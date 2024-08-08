264b15b80f898ce4171c804b5c0ba2ba64f54fd1
PriyankaHP1604 <166796771+PriyankaHP1604@users.noreply.github.com>
2024-08-08 17:42:00 +0530

    Update HeroSection.jsx

--- vite-project/src/components/heroSection/HeroSection.jsx
+++ vite-project/src/components/heroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div>
       <img
-        src="src\assets\Herosection.png"
+        src="https://st3.depositphotos.com/12985790/15892/i/450/depositphotos_158920530-stock-photo-family-running-with-shopping-bags.jpg"
         alt=""
         style={{ width: '100%' }}
       />
